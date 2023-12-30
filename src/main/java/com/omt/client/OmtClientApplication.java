6c4b157cf31f5ffedd03cca274680f8754d52926
owen <owen.chen@cloud-bird.com>
2023-12-30 15:27:43 +0800

    commit

---
+++ src/main/java/com/omt/client/OmtClientApplication.java
@@ -0,0 +1,13 @@
+package com.omt.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmtClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmtClientApplication.class, args);
+	}
+
+}
